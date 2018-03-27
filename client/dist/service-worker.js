230a7f3186f4e48390f6bbf5cfef8ca83d1cb27e
Joe Ford <joefordiv7@gmail.com>
2018-03-26 21:21:38 -0400

    test

--- client/dist/service-worker.js
+++ client/dist/service-worker.js
@@ -1,11 +1,12 @@
-importScripts ('/cache-polyfill.js');
+importScripts('/cache-polyfill.js');
 
-(function() {
+(function () {
   self.addEventListener('install', event => {
     console.log('Service worker installing...');
     self.skipWaiting();
     event.waitUntil(
       caches
+<<<<<<< HEAD
         .open('getFixed')
         .then(cache => {
           return cache.addAll([
@@ -16,6 +17,17 @@ importScripts ('/cache-polyfill.js');
           ]);
         })
         .catch(err => console.log('Install Error:', err))
+=======
+      .open('getFixed')
+      .then(cache => {
+        // console.log(cache);
+        return cache.addAll([
+          'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css',
+          '/bundle.js'
+        ]);
+      })
+      .catch(err => console.log('Install Error:', err))
+>>>>>>> test
     );
   });
 
@@ -24,9 +36,11 @@ importScripts ('/cache-polyfill.js');
   self.addEventListener('fetch', event => {
     event.respondWith(
       caches
-        .match(event.request)
-        .then(response => { return response || fetch(event.request); })
-        .catch(err => console.log('Fetch Error:', err))
+      .match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(err => console.log('Fetch Error:', err))
     );
   });
 })();
